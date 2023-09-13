b233f7d7a24c6effd0f6fffa70d6af36c91a9173
Marcia Ramos <13464393+VirtuaCreative@users.noreply.github.com>
2023-09-13 18:18:03 +0100

    Add dev.js for local development

--- index.js
+++ index.js
@@ -3,8 +3,3 @@ const app = express();
 const path = require('path');
 
 express.static(path.join(__dirname, '/_site'));
-
-// Uncomment for local development
-// app.listen(8080, () => {
-// 	console.log('Jekyll Static Site listening on port http://localhost:8080');
-// });
