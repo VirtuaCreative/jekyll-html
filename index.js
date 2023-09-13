0bb94188683914182942861bd624152b94c16ea0
Marcia Ramos <13464393+VirtuaCreative@users.noreply.github.com>
2023-09-13 11:27:47 +0100

    Add app.get function back

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'index.html');
+});
+
+// Uncomment for local development
 // app.listen(8080, () => {
 // 	console.log(`Jekyll Static Site listening on port 8080!`);
 // });
